7cf9c15f091551841ea11c114121c73fc1de10c0
xiaohongbing <m18779334084@163.com>
2020-07-15 00:27:39 +0800

    完成底层导航栏

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// const Home = () => import('../views/home/Home')
+// const Category = () => import('../views/category/Category')
+// const Cart = () => import('../views/cart/Cart')
+// const Profile = () => import('../views/profile/Profile')
+
+// 1.安装插件
+Vue.use(VueRouter)
+
+// 2.创建router
+const routes = [
+  // {
+  //   path: '',
+  //   redirect: '/home'
+  // },
+  // {
+  //   path: '/home',
+  //   component: Home
+  // }
+]
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+
+export default router
